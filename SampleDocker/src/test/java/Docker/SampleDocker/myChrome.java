2706694a01a1db185ee8561579e2dac4b80df7fe
yogeshanthati <yogesh946@gmail.com>
2020-02-07 12:03:55 +0530

    docker

---
+++ SampleDocker/src/test/java/Docker/SampleDocker/myChrome.java
@@ -0,0 +1,31 @@
+package Docker.SampleDocker;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class myChrome {
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+DesiredCapabilities dc =DesiredCapabilities.chrome();
+
+URL url=new URL("http://192.168.99.100:4444/wd/hub");
+
+RemoteWebDriver driver=new RemoteWebDriver(url, dc);
+
+driver.get("https://www.facebook.com/");
+
+System.out.println(driver.getTitle());
+
+driver.quit();
+	}
+	
+	
+	
+	
+//	Reference:
+	//URL---->https://www.youtube.com/watch?v=UzwnM13Na8E&list=PLUDwpEzHYYLtpm24ojHwubwmMNQildHBc&index=5
+}
---
+++ SeleniumGridwith docker/SampleDocker/src/test/java/Docker/SampleDocker/myFirefox.java
@@ -0,0 +1,26 @@
+package Docker.SampleDocker;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class myFirefox {
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+DesiredCapabilities dc =DesiredCapabilities.firefox();
+
+URL url=new URL("http://192.168.99.100:4444/wd/hub");
+
+RemoteWebDriver driver=new RemoteWebDriver(url, dc);
+
+driver.get("https://www.facebook.com/");
+
+System.out.println(driver.getTitle());
+
+driver.quit();
+	
+	}
+
+}
